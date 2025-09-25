699d734a43cbdf835e172ebd5fdeebaf9e069dae
Thongsavanh Malikai <826764@stma.k12.mn.us>
2025-09-25 14:03:16 -0500

    inital commit

---
+++ Assignments/SpaceShuttleSupply.py
@@ -0,0 +1,17 @@
+countdown = input("How many seconds are left until launch\n> ")
+countdown = int(countdown)
+
+
+oxygen_tanks = input("How many oxygen tanks are required in the mission\n> ")
+
+
+
+food_packs = ("How many foods packs do you have\n> ")
+
+
+water_packs = ("How many water packs do you have stored\n> ")
+print(countdown * countdown)
+print(oxygen_tanks)
+print(food_packs)
+
+print(water_packs)
--- Practice/2.3-practice.py
+++ Practice/2.3-practice.py
@@ -11,3 +11,12 @@ SCREAMING_SNAKE_CASE = False
 #1. Concise 
 the_font_size_for_paragraph = 10 #bad one
 font_size_para = 10 #Good one 
+
+# get the number as a string 
+num = input("What number do you want to square\n> ")
+
+#Parse (Convert} the string to an interger cast, int converts string to itnerger, float, boolean, str also work
+num = int(num)
+
+#do math and print
+print(num * num)

1f1183136a50c2236b6e8a2ea59b48aa2f47778f
Thongsavanh Malikai <826764@stma.k12.mn.us>
2025-10-01 13:03:11 -0500

    inital commit

--- Practice/2.4-practice.py
+++
@@ -1,7 +0,0 @@
-favorite_animal = input("What's your favorite animal?" )
-why_animal = input("Why is this your favorite animal\n> ")
-sound_animal = input("What sound does the animal make?\n> ")
-looks_animal = input("What does this animal look like?\n> ")
-print("Your favorite animal is"  + favorite_animal + "" + "Because" + str(why_animal) + "" "They make a" + str(sound_animal) + ("They look") + str(looks_animal))
-
-

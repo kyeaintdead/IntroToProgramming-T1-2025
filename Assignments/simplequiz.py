068e353706976084ddcfc3a870afe131b5cd8bd4
Thongsavanh Malikai <826764@stma.k12.mn.us>
2025-10-09 13:41:04 -0500

    updated

--- Assignments/simplequiz.py
+++ Assignments/simplequiz.py
@@ -1,49 +1,40 @@
 
-answer = input("Give me the infamous number\n>")
+answer1 = input("Give me the infamous number\n>")
+
+answer2 = input("Which one comes first Milk or Cereal\n>")
 
 
-answer = input("Which one comes first Milk or Cereal\n>")
+answer3 = input("What School are you from STMA or somewhere else\n>")
 
 
-answer = input("What School are you from STMA or somewhere else\n>")
+answer4 =input("What is the sum of 2 + 2\n>")
 
 
-answer = input("What is the sum of 2 + 2\n>")
-
-
-answer = input("What comes first the chicken or the egg\n>")
+answer5 = input("What comes first the chicken or the egg\n>")
 
 
 def simplequiz():
-    answer = "67"
-    answer = "Cereal"
-    answer = "STMA"
-    answer = "Nike"
-    answer = "chicken"
+    score = 0 
+   
 
 
-    if answer == "Nike":
-        print("You got it")
-    else:
-        print("Wrong bozo")
+    if answer4 == "Nike":
+       score = score + 1
+    
        
-    if answer == "67":
-         print("You got it")
-    else: 
-            print("Wrong bozo")
+    if answer1 == "67":
+         score = score + 1
+    
     
 
-    if answer == "Cereal":
-        print("You're a normal person good job")
-    else:
-        print("Wrong try again bozo")
-    if answer == "STMA":
-        print("You are form stma")
-    else:
-         print("You are from another school")
-    if answer == "chicken":
-         print("You got it")
-    else:
-         print("You got it wrong")
-         simplequiz()
+    if answer2 == "Cereal":
+        score = score + 1
+   
+    if answer3 == "STMA":
+        score = score + 1 
+    
+    if answer5 == "chicken":
+         score = score + 1
+    
+    print("SCORE:  "  + str(score) + "/5")
 simplequiz()
